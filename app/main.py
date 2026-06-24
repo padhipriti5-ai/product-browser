6f2a57b3e5fac1bda2458d70b9faa8ded420c66f
padhipriti5-ai <padhipriti5@gmail.com>
2026-06-24 20:28:10 +0530

    fix cors middleware

--- app/main.py
+++ app/main.py
@@ -10,12 +10,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://product-browser-plum.vercel.app"
-]
+        "http://127.0.0.1:5173",
+        "https://product-browser-plum.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
