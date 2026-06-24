6da938677e6e312de441263928991a53b162ad35
padhipriti5-ai <padhipriti5@gmail.com>
2026-06-24 22:14:30 +0530

    fix cors for vercel domain

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://product-browser-plum.vercel.app"
+        "https://product-browser-three.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
