eb00710f64060eb137824b8ff935c6f8846db098
padhipriti5-ai <padhipriti5@gmail.com>
2026-06-24 16:59:01 +0530

    add vercel cors

--- app/main.py
+++ app/main.py
@@ -11,7 +11,11 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173"
-    ],
+    ],allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://product-browser-plum.vercel.app"
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
